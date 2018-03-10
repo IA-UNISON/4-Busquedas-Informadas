e91d47fb80c77e3e052253903a341ab56921552d
Giovanni Lopez Celaya <giovanni.lopez3523@gmail.com>
2018-03-09 23:02:03 -0700

    Se me olvido el fork :v estaba trabjando de un clon del original

--- lightsout.py
+++ lightsout.py
@@ -7,7 +7,7 @@ lightsout.py
 Tarea sobre búsquedas, donde lo que es importante es crear nuevas heurísticas
 
 """
-__author__ = 'nombre del estudiante'
+__author__ = 'Giovanni Lopez Celaya'
 
 
 import busquedas
@@ -19,7 +19,7 @@ class LightsOut(busquedas.ModeloBusqueda):
     # para el modelo de lights out
     # --------------------------------------------------------
     """
-    Problema del jueguito "Ligths out".
+    Problema del jueguito "Lights out".
 
     La idea del juego es el apagar o prender todas las luces.
     Al seleccionar una casilla, la casilla y sus casillas
@@ -56,12 +56,26 @@ class LightsOut(busquedas.ModeloBusqueda):
         raise NotImplementedError('Hay que hacerlo de tarea')
 
     def acciones_legales(self, estado):
+        return range(0,25)
         raise NotImplementedError('Hay que hacerlo de tarea')
 
     def sucesor(self, estado, accion):
+        estado_nuevo=list(estado)
+        estado_nuevo[accion]=1-estado_nuevo[accion]  
+        if accion%5 < 4:
+            estado_nuevo[accion+1]=1-estado_nuevo[accion+1]
+        if accion%5 > 0:
+            estado_nuevo[accion-1]=1-estado_nuevo[accion-1]
+        if accion//5 >0:
+            estado_nuevo[accion-5]=1-estado_nuevo[accion-5]
+        if accion//5 < 4:
+            estado_nuevo[accion+5]=1-estado_nuevo[accion+5]
+        
+        return tuple(estado_nuevo)
         raise NotImplementedError('Hay que hacerlo de tarea')
 
     def costo_local(self, estado, accion):
+        return 1
         raise NotImplementedError('Hay que hacerlo de tarea')
 
     @staticmethod
@@ -85,15 +99,21 @@ class LightsOut(busquedas.ModeloBusqueda):
 #  Problema 3: Completa el problema de LightsOut
 # ------------------------------------------------------------
 class ProblemaLightsOut(busquedas.ProblemaBusqueda):
-    def __init__(self, pos_ini):
+    def __init__(self,pos_ini):
         """
         Utiliza la superclase para hacer el problema
-
         """
         # Completa el código
+        
         x0 = tuple(pos_ini)
         def meta(x):
-            raise NotImplementedError("Hay que hacer de tarea")
+            meta = True
+            for i in x:
+                if i == 1:
+                    meta= False
+                    break
+            return meta
+            #raise NotImplementedError("Hay que hacer de tarea")
 
         super().__init__(x0=x0, meta=meta, modelo=LightsOut())
 
@@ -105,9 +125,23 @@ def h_1(nodo):
     """
     DOCUMENTA LA HEURÍSTICA QUE DESARROLLES Y DA UNA JUSTIFICACIÓN
     PLATICADA DE PORQUÉ CREES QUE LA HEURÍSTICA ES ADMISIBLE
-
     """
-    return 0
+    """
+    Siendo lo mas optimista posible es asumir que lo que nos falta para 
+    llegar a la solucion son el numero de luces encendidas al igual que en 
+    el 8-puzzle y funciona en friega.
+    """
+    
+    return sum(nodo.estado)
+    
+    """    
+    suma=0
+    for x in range(5):
+        for y in range(5):
+            if(y==x and nodo.estado[x]==1):
+                suma+=1
+    return suma
+    """
 
 
 # ------------------------------------------------------------
@@ -119,15 +153,25 @@ def h_2(nodo):
     """
     DOCUMENTA LA HEURÍSTICA DE DESARROLLES Y DA UNA JUSTIFICACIÓN
     PLATICADA DE PORQUÉ CREES QUE LA HEURÍSTICA ES ADMISIBLE
-
-    """
-    return 0
-
+    
+    Sumamos el contorno de las orillas del tablero debido a que
+    es donde surgen los mayores problemas al tratar de terminar
+    el juego, entonceces al tener las orillas en 0 se podria decir 
+    que es el problema del juego ya que el centro siempre esta 
+    cambiando.
+    
+    AL parecer h_1 domina a h_2 puesto que revisa menos nodos
+    
+    """        
+    n1=nodo.estado[24]+nodo.estado[23]+nodo.estado[22]+nodo.estado[21]
+    n2=nodo.estado[0]+nodo.estado[1]+nodo.estado[2]+nodo.estado[3]
+    n3=nodo.estado[4]+nodo.estado[8]+nodo.estado[12]+nodo.estado[16]
+    n4=nodo.estado[20]+nodo.estado[15]+nodo.estado[10]+nodo.estado[5]
+    return n1+n2+n3+n4
 
 def prueba_modelo():
     """
     Prueba la clase LightsOut
-
     """
 
     pos_ini = (0, 1, 0, 1, 0,
@@ -181,16 +225,12 @@ def compara_metodos(pos_inicial, heuristica_1, heuristica_2):
     """
     Compara en un cuadro lo nodos expandidos y el costo de la solución
     de varios métodos de búsqueda
-
     @param pos_inicial: Una tupla con una posicion inicial
     @param heuristica_1: Una función de heurística
     @param heuristica_2: Una función de heurística
-
     @return None (no regresa nada, son puros efectos colaterales)
-
     Si la búsqueda no informada es muy lenta, posiblemente tendras que quitarla
     de la función
-
     """
     solucion1 = busquedas.busqueda_A_estrella(ProblemaLightsOut(pos_inicial),
                                               heuristica_1)
@@ -235,11 +275,12 @@ if __name__ == "__main__":
     print("\n\nPara el problema en diagonal")
     print("\n{}".format(LightsOut.bonito(diagonal)))
     compara_metodos(diagonal, h_1, h_2)
-
-    print("\n\nPara el problema simétrico")
-    print("\n".format(LightsOut.bonito(simetria)))
+    
+    print("\n\nPara el problema en simétrico")
+    print("\n{}".format(LightsOut.bonito(simetria)))
     compara_metodos(simetria, h_1, h_2)
-
-    print("\n\nPara el problema Bonito")
-    print("\n".format(LightsOut.bonito(problemin)))
+    
+    
+    print("\n\nPara el problema bonito")
+    print("\n{}".format(LightsOut.bonito(problemin)))
     compara_metodos(problemin, h_1, h_2)
