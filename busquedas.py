18f344fa3a069a156b8b9873b4acec6d34ff7ada
JhonSmith <jhonsmith@localhost.localdomain>
2018-03-11 10:27:02 -0700

    Implementa a*
    Implementa el algoritmo a*.
    
    Tenia la intencion de basarme en el algoritmo de wikipedia pero al
    final parecio mas una busqueda de costo uniforme y siento que varias
    lineas del algoritmo de wikipedia fueron evitadas al usar el heapq.

--- busquedas.py
+++ busquedas.py
@@ -282,7 +282,91 @@ def busqueda_A_estrella(problema, heuristica):
                        un nodo cuyo estado final sea méta.
 
     @return Un objeto tipo Nodo con la estructura completa
+    """
 
     """
-    raise NotImplementedError('Hay que hacerlo de tarea \
-                              (problema 2 en el archivo busquedas.py)')
+    Pseudo codigo de wikipedia para guiarme:
+
+     // The set of nodes already evaluated
+    closedSet := {}
+
+    // The set of currently discovered nodes that are not evaluated yet.
+    // Initially, only the start node is known.
+    openSet := {start}
+
+    // For each node, which node it can most efficiently be reached from.
+    // If a node can be reached from many nodes, cameFrom will eventually contain the
+    // most efficient previous step.
+    cameFrom := an empty map
+
+    // For each node, the cost of getting from the start node to that node.
+    gScore := map with default value of Infinity
+
+    // The cost of going from start to start is zero.
+    gScore[start] := 0
+
+    // For each node, the total cost of getting from the start node to the goal
+    // by passing by that node. That value is partly known, partly heuristic.
+    fScore := map with default value of Infinity
+
+    // For the first node, that value is completely heuristic.
+    fScore[start] := heuristic_cost_estimate(start, goal)
+
+    while openSet is not empty
+        current := the node in openSet having the lowest fScore[] value
+        if current = goal
+            return reconstruct_path(cameFrom, current)
+
+        openSet.Remove(current)
+        closedSet.Add(current)
+
+        for each neighbor of current
+            if neighbor in closedSet
+                continue		// Ignore the neighbor which is already evaluated.
+
+            if neighbor not in openSet	// Discover a new node
+                openSet.Add(neighbor)
+
+            // The distance from start to a neighbor
+            //the "dist_between" function may vary as per the solution requirements.
+            tentative_gScore := gScore[current] + dist_between(current, neighbor)
+            if tentative_gScore >= gScore[neighbor]
+                continue		// This is not a better path.
+
+            // This path is the best until now. Record it!
+            cameFrom[neighbor] := current
+            gScore[neighbor] := tentative_gScore
+            fScore[neighbor] := gScore[neighbor] + heuristic_cost_estimate(neighbor, goal)
+
+    return failure
+
+function reconstruct_path(cameFrom, current)
+    total_path := [current]
+    while current in cameFrom.Keys:
+        current := cameFrom[current]
+        total_path.append(current)
+    return total_path
+
+    """
+
+    #parece ser casi identico a una busqueda uniforme, la unica diferencia es que al meter
+    #los costos a la frontera se toma en cuenta tambien la heuristica
+    frontera = []
+    heapq.heappush(frontera, (0, Nodo(problema.x0)))
+    visitados = {problema.x0: 0}
+
+    while frontera:
+        (_, nodo) = heapq.heappop(frontera)
+        if problema.es_meta(nodo.estado):
+            nodo.nodos_visitados = problema.num_nodos
+            return nodo
+
+        for hijo in nodo.expande(problema.modelo):
+            if (hijo.estado visitados or hijo.costo >= visitados[hijo.estado]):
+                continue
+
+            visitados[hijo.estado] = hijo.costo
+            heapq.heappush(frontera, (hijo.costo + heuristica(hijo), hijo))
+
+    return None
+
