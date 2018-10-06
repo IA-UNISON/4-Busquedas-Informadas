faf5986e4b7d1738960d6e6c817cd9b78e1eb981
Gilberto Espinoza <gilbertoespinoza97@gmail.com>
2018-10-05 19:01:35 -0700

    A* funcionando con Ocho puzzles

--- busquedas.py
+++ busquedas.py
@@ -13,7 +13,7 @@ completamente observables
 __author__ = 'juliowaissman'
 
 from collections import deque
-from queue import PriorityQueue
+import heapq
 
 class ModeloBusqueda:
     """
@@ -267,37 +267,8 @@ def busqueda_costo_uniforme(problema):
 # dificl (en el archivo se incluyen las heurísticas del 8 puzzle y el
 # resultado esperado)
 #
-# Referencia: https://github.com/DumbBrain/AI_modern_approach/blob/master/search.py
-#
-# ---------------------------------------------------------------------
-def best_first_graph_search(problem, f):
-    """Search the nodes with the lowest f scores first.
-    You specify the function f(node) that you want to minimize; for example,
-    if f is a heuristic estimate to the goal, then we have greedy best
-    first search; if f is node.depth then we have breadth-first search.
-    There is a subtlety: the line "f = memoize(f, 'f')" means that the f
-    values will be cached on the nodes as they are computed. So after doing
-    a best first search you can examine the f values of the path returned."""
-    f = memoize(f, 'f')
-    node = Node(problem.initial)
-    frontier = PriorityQueue('min', f)
-    frontier.append(node)
-    explored = set()
-    while frontier:
-        node = frontier.pop()
-        if problem.goal_test(node.state):
-            return node
-        explored.add(node.state)
-        for child in node.expand(problem):
-            if child.state not in explored and child not in frontier:
-                frontier.append(child)
-            elif child in frontier:
-                incumbent = frontier[child]
-                if f(child) < f(incumbent):
-                    del frontier[incumbent]
-                    frontier.append(child)
-return None
 
+#
 
 # Metodo memoize - https://www.python-course.eu/python3_memoization.php
 def memoize(fun):
@@ -312,6 +283,7 @@ def memoize(fun):
 
 
 def busqueda_primero_mejor(problema, f):
+    # Referencia: https://github.com/DumbBrain/AI_modern_approach/blob/master/search.py
     """
         Busca los nodos con la mejor heuristica, la menor
         Se especifica la funcion que se quiere minimizar (f).
@@ -323,32 +295,32 @@ def busqueda_primero_mejor(problema, f):
 
         memoize significa que los valores seran guardados en un cache, como son computados. Entonces despues de hacer busqueda podemos recuperar el camino.
     """
-    f = memoize(f)
-    nodo = Nodo(problema.x0)
-    # Hacemos que la frontera ordene los nodos, partiendo del min, de la funcion f - https://dbader.org/blog/priority-queues-in-python
-    frontera = PriorityQueue(f)
-    frontera.put(nodo)
-    explorados = set()
+    f = memoize(f) #optimizamos el calculo de la heuristica con memoize
+
+    #pusheamos tublas en una lista mientras que heapq la mantiene como lista ordenada
+    frontera = []
+    heapq.heappush(frontera, (0, Nodo(problema.x0))) # inicamos caminos
+    # Refenrencia - https://docs.python.org/3/library/heapq.html
+
+    visitados = {problema.x0 : 0} # diccionario de nodos ya visitados
 
     while frontera:
-        nodo = frontera.get()
+        # retiramos un nodo, el costo esta dentro del nodo
+        (_, nodo) = heapq.heappop(frontera)
+
         if problema.es_meta(nodo.estado):
+            nodo.nodos_visitados = problema.num_nodos
             return nodo
-        explorados.add(nodo.estado)
-        for hijo in nodo.expande(problema):
-            if hijo.estado not in explorados and hijo not in frontera:
-                frontera.put(hijo)
-            # dos caminos para un mismo nodo
-            elif hijo in frontera:
-                # nodo ya encontrado con el mismo hijo.estado
-                aux = frontera[hijo]
-                # comparamos heurista -
-                if f(hijo) < f(aux):
-                    # borramos al aux si tiene peor heuristica https://stackoverflow.com/questions/11520492/difference-between-del-remove-and-pop-on-lists/11520540
-                    del frontera[aux]
-                    frontera.put(hijo)
+
+        for hijo in nodo.expande(problema.modelo):
+            if hijo.estado not in visitados:
+                #pusheamos su f() y el nodo
+                heapq.heappush(frontera, ( f(hijo), hijo) ) # funcion solo marca la formula que recibimos. para A* enviamos una funcion lambda que incluye el costo del camino y la heuristica.
+                visitados[hijo.estado] = hijo.costo
+            elif hijo.costo < visitados[hijo.estado]: # si el hijo ya esta en vistidaos hay que verificar f y si es menor, pushearlo
+                heapq.heappush(frontera, ( f(hijo), hijo) )
     # No salimos hasta que hay solucion,,,,
-return None
+    return None
 
 
 # ---------------------------------------------------------------------
@@ -363,9 +335,19 @@ def busqueda_A_estrella(problema, heuristica):
                        un nodo cuyo estado final sea méta.
 
     @return Un objeto tipo Nodo con la estructura completa
-
     """
-    frontera = []
+    #--------------------------------------
+    """
+    def astar_search(problem, h=None):
+        A* search is best-first graph search with f(n) = g(n)+h(n).
+        You need to specify the h function when you call astar_search, or
+        else in your Problem subclass
+        h = memoize(h or problem.h, 'h')
+    return best_first_graph_search(problem, lambda n: n.path_cost + h(n))
+    """
+    #--------------------------------------
+    h = memoize(heuristica)
+    return busqueda_primero_mejor(problema, lambda n: n.costo + h(n))
 
     raise NotImplementedError('Hay que hacerlo de tarea \
                               (problema 2 en el archivo busquedas.py)')
