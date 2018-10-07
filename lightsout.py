99d5b4ffd43def33953a7575e1262354869ed3ef
Gilberto Espinoza <gilbertoespinoza97@gmail.com>
2018-10-06 19:18:09 -0700

    Lights Out

--- lightsout.py
+++ lightsout.py
@@ -149,6 +149,7 @@ def h_1(nodo):
     DOCUMENTA LA HEURÍSTICA QUE DESARROLLES Y DA UNA JUSTIFICACIÓN
     PLATICADA DE PORQUÉ CREES QUE LA HEURÍSTICA ES ADMISIBLE
 
+    SUMA LUCES ENCEDIDAS
     La heuristica no es admisible pues si tuvieramos una cruz aislada,
     la heuristica regresa 5, pero el costo real a la solucion es 1
     Aun asi esta heuristica encuentra solucion
@@ -177,7 +178,8 @@ def h_2(nodo):
     Esta heuristca regresa maximo 2, es decir, existen cruces aisladas en [6,18] o [8, 16],
     por lo cual nunca sobreestima, pues si solo estan encendidas estas cruces, bastan
     dos movimientos (los centros de las cruces) para apagar todas estas.
-
+    Especifico que sea aislada por que de no serlo, es posible que haya un mejor
+    camino para resolver esa cruz respecto a la cuadricula
     ---------------------
     |   | X |   |   |   |
     ---------------------
@@ -189,11 +191,15 @@ def h_2(nodo):
     ---------------------
     |   |   |   |   |   |
     ---------------------
+        ESQUINAS AISLADAS - heuristica analoga
+        De igual manera que las cruces aisladas las esquinas, se pueden activar o
+        desactivar con un click. Combinar ambas heuristica sigue siendo admisible
+        pues pueden estar de lados opuestos del mapa y regresarian 2, un click por
+        cada patron encontrado
     """
     def cruz_aislada(nodo):
         """
-            estado: tupla con el estado de la cuadricula
-            accion: casilla a verificar si es una cruz aisladas
+            nodo: el nodo.
         """
 
         # centro de la cuadricula 5x5
@@ -228,6 +234,66 @@ def h_2(nodo):
     h_2 += cruz_aislada(nodo)
 
     return h_2
+
+def h_3(nodo):
+    """
+    DOCUMENTA LA HEURÍSTICA DE DESARROLLES Y DA UNA JUSTIFICACIÓN
+    PLATICADA DE PORQUÉ CREES QUE LA HEURÍSTICA ES ADMISIBLE
+
+    DIFERENCIA DE ESTADOS (LUCES)
+    Heuristica que checa cuantas luces se encendieron de ir del nodo.padre
+    al nodo
+    El maximo es +/- 5 por accion, y solo pasa si es cruz aislada. Se prende o
+    se apaga la cruz completa
+    Debemos tomar en cuenta si la diferencia es negativa, pues eso
+    significa que nodo tiene mas luces encendidas que su padre, y eso no lo queremos
+
+    En la implementacion de esta heuristica note que la misma no es admisible,
+    pues si una accion enciende una cruz aislada, necesitamos un paso para
+    desactivarla pero la heuristica regresara 5, lo cual prueba que no es admisible
+
+    Razonando el punto anterior, eso solo sucede cuando tenemos encedemos una cruz aislada,
+    entonces si luces_encendidas = 5, se regresara 1, definiendo que necesitamos un paso mas
+
+    Entonces nuestra heuristica es admisible hasta el momento.
+    """
+    # Nos robamos nuestro propio codigo implementado en sucesor, solo queremos saber los vecinos de la accion del nodo
+    vecinos = []
+    vecinos.append(0) # de ley se apaga o se prendre la casilla de accion
+    # vericamos si esta en un borde, para definir la vecindad
+    dim = 5 #NUMERO_MAGICO
+    if nodo.accion // 5 is not 4: vecinos.append(dim)    #NUMERO_MAGICO
+    if nodo.accion // 5 is not 0: vecinos.append(-dim)   #NUMERO_MAGICO
+    if nodo.accion % 5 is not 4: vecinos.append(+1)    #NUMERO_MAGICO
+    if nodo.accion % 5 is not 0: vecinos.append(-1)    #NUMERO_MAGICO
+
+    # La accion es como ir de nodo.padre a nodo
+    luces_encendidas = 0 # Suponemos el mejor caso que la accion desactivo una cruz esquina o borde aislado
+
+    for i in range(0,len(vecinos)):
+        # Si en nodo padre la casilla estaba apgada y ahora esta encendida, nuestra heuristica suma 5
+        if nodo.padre.estado[nodo.accion + vecinos[i]] - nodo.estado[nodo.accion + vecinos[i]] == -1:
+            luces_encendidas += 1
+
+    if luces_encendidas == 5: # vericamos cruz
+        return 1
+
+    return luces_encendidas
+
+    """ primer acercamiento a la heuristica, rechazada por memoize()
+    luces_apagadas = (sum(nodo.padre.estado) - sum(nodo.estado))
+    print("Diferencia: {}".format(luces_apagadas))
+    if luces_apagadas < 0:
+        # Dado que es negativo, lo transformamos a positivo para que cuente la heuristica
+        return (luces_apagadas * -1)
+
+    File "/home/gilbertoesp/Documents/inteligencia_artificial/tareas/4-Busquedas-Informadas/busquedas.py", line 279, in _fun
+        y = fun(x)
+    File "/home/gilbertoesp/Documents/inteligencia_artificial/tareas/4-Busquedas-Informadas/busquedas.py", line 350, in <lambda>
+        return busqueda_primero_mejor(problema, lambda n: n.costo + h(n))
+    TypeError: unsupported operand type(s) for +: 'int' and 'NoneType'
+    """
+
 
 def prueba_modelo():
     """
@@ -282,7 +348,7 @@ def prueba_modelo():
     print("Paso la prueba de la clase LightsOut")
 
 
-def compara_metodos(pos_inicial, heuristica_1, heuristica_2):
+def compara_metodos(pos_inicial, heuristica_1, heuristica_2, heuristica_3):
     """
     Compara en un cuadro lo nodos expandidos y el costo de la solución
     de varios métodos de búsqueda
@@ -298,9 +364,12 @@ def compara_metodos(pos_inicial, heuristica_1, heuristica_2):
 
     """
     solucion1 = busquedas.busqueda_A_estrella(ProblemaLightsOut(pos_inicial),
-                                              heuristica_1)
+                                            heuristica_1)
     solucion2 = busquedas.busqueda_A_estrella(ProblemaLightsOut(pos_inicial),
-                                              heuristica_2)
+                                            heuristica_2)
+
+    solucion3 = busquedas.busqueda_A_estrella(ProblemaLightsOut(pos_inicial),
+                                            heuristica_3)
 
     print('-' * 50)
     print('Método'.center(10) + 'Costo'.center(20) + 'Nodos visitados')
@@ -309,11 +378,25 @@ def compara_metodos(pos_inicial, heuristica_1, heuristica_2):
           str(solucion1.nodos_visitados))
     print('A* con h2'.center(10) + str(solucion2.costo).center(20) +
           str(solucion2.nodos_visitados))
+    print('A* con h3'.center(10) + str(solucion3.costo).center(20) +
+          str(solucion3.nodos_visitados))
     print('-' * 50 + '\n\n')
 
 
 if __name__ == "__main__":
+"""
+    h_3 es dominante respecto a h_2, pues h_3, toma en cuenta la exitensia de las cruces
+    aisladas o no y le da una heuristica de 1, lo mismo que h_2
 
+    Comentarios finales:
+    Agrego distintas salidas de mi tarea
+    Algunas secciones del codigo pueden ser codificadas de forma mas limpia
+    luego de ver su implementacion mas burda y tosca. Preferi dejarla asi por motivos
+    de tiempo, ejemplo, como encuentro a los vecinos, #NUMERO_MAGICO por mencionar algunas 
+
+    En la corrida del script, podemos observar que a pesar h_1 no es admisible, es la mejor
+    y que h_3 tiene menos nodos que h_2, casi siempre
+"""
     print("Antes de hacer otra cosa,")
     print("vamos a verificar medianamente la clase LightsOut")
     prueba_modelo()
@@ -339,12 +422,12 @@ if __name__ == "__main__":
 
     print("\n\nPara el problema en diagonal")
     print("\n{}".format(LightsOut.bonito(diagonal)))
-    compara_metodos(diagonal, h_1, h_2)
+    compara_metodos(diagonal, h_1, h_2, h_3)
 
     print("\n\nPara el problema simétrico")
     print("\n".format(LightsOut.bonito(simetria)))
-    compara_metodos(simetria, h_1, h_2)
+    compara_metodos(simetria, h_1, h_2, h_3)
 
     print("\n\nPara el problema Bonito")
     print("\n".format(LightsOut.bonito(problemin)))
-    compara_metodos(problemin, h_1, h_2)
+    compara_metodos(problemin, h_1, h_2, h_3)
