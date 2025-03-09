d244964d1285da0883a2d8aaa3d63c92a462a4a8
FranciscoAtondo <atondofrancisco@hotmail.com>
2025-03-09 14:56:11 -0600

    Intentado, pero no funciono el Cubo Rubik

--- problemas.py
+++ problemas.py
@@ -171,16 +171,87 @@ class CuboRubik(busquedas.ModeloBusqueda):
     B = Blanco
     C = Cafe
 
-    
+    Las acciones de este se dictan de la siguiente manera:
+
+    0 - Mover la fila superior de cuadros horizontalmente a la derecha
+    1 - Mover la fila central de cuadros horizontalmente a la derecha
+    2 - Mover la fila inferior de cuadros horizontalmente a la derecha
+    3 - Mover la fila superior de cuadros horizontalmente a la izquierda
+    4 - Mover la fila central de cuadros horizontalmente a la izquierda
+    5 - Mover la fila inferior de cuadros horizontalmente a la izquierda
+    6 - Mover la columna izquierda de cuadros verticalmente hacia arriba
+    7 - Mover la columna central de cuadros verticalmente hacia arriba
+    8 - Mover la columna derecha de cuadros verticalmente hacia arriba
+    9 - Mover la columna izquierda de cuadros verticalmente hacia abajo
+    10 - Mover la columna central de cuadros verticalmente hacia abajo
+    11 - Mover la columna derecha de cuadros verticalmente hacia abajo
+    12 - Girar la cara frontal del cubo hacia la izquierda
+    13 - Girar la cara frontal del cubo hacia la derecha
     """
     def __init__(self):
-        raise NotImplementedError('Hay que hacerlo de tarea')
-
+        pass
+    
     def acciones_legales(self, estado):
-        aux = A
-
+        """ Devuelve la lista de acciones legales desde un estado dado."""
+        return list(range(14))
+    
     def sucesor(self, estado, accion):
-        raise NotImplementedError('Hay que hacerlo de tarea')
+        """ Devuelve un nuevo estado tras aplicar una acción."""
+        nuevo_estado = [list(cara) for cara in estado]
+        
+        if accion in range(6):  # Movimientos de filas
+            self.mover_fila(nuevo_estado, accion)
+        elif accion in range(6, 12):  # Movimientos de columnas
+            self.mover_columna(nuevo_estado, accion)
+        elif accion in [12, 13]:  # Giros de la cara frontal
+            self.girar_frontal(nuevo_estado, accion)
+        
+        return tuple(tuple(cara) for cara in nuevo_estado)
+    
+    def mover_fila(self, estado, accion):
+        """ Realiza un movimiento horizontal en la fila superior, central o inferior."""
+        fila = accion % 3  # 0 = superior, 1 = central, 2 = inferior
+        derecha = accion < 3
+        
+        if derecha:
+            temp = estado[3][fila][:]
+            estado[3][fila] = estado[2][fila]
+            estado[2][fila] = estado[1][fila]
+            estado[1][fila] = estado[0][fila]
+            estado[0][fila] = temp
+        else:
+            temp = estado[0][fila][:]
+            estado[0][fila] = estado[1][fila]
+            estado[1][fila] = estado[2][fila]
+            estado[2][fila] = estado[3][fila]
+            estado[3][fila] = temp
+    
+    def mover_columna(self, estado, accion):
+        """ Realiza un movimiento vertical en la columna izquierda, central o derecha."""
+        col = (accion - 6) % 3
+        arriba = accion < 9
+        
+        if arriba:
+            temp = estado[4][col][:]
+            estado[4][col] = estado[3][col]
+            estado[3][col] = estado[5][col]
+            estado[5][col] = estado[1][col]
+            estado[1][col] = temp
+        else:
+            temp = estado[1][col][:]
+            estado[1][col] = estado[5][col]
+            estado[5][col] = estado[3][col]
+            estado[3][col] = estado[4][col]
+            estado[4][col] = temp
+    
+    def girar_frontal(self, estado, accion):
+        """ Gira la cara frontal del cubo a la izquierda o derecha."""
+        derecha = accion == 13
+        
+        if derecha:
+            estado[0] = [estado[0][2], estado[0][0], estado[0][3], estado[0][1]]
+        else:
+            estado[0] = [estado[0][1], estado[0][3], estado[0][0], estado[0][2]]
 
     def costo_local(self, estado, accion):
         normal = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}
@@ -199,8 +270,10 @@ class PblCuboRubik(busquedas.ProblemaBusqueda):
     El problema a resolver es establecer un plan para resolver el cubo de rubik.
 
     """
-    def __init__(self):
-        raise NotImplementedError('Hay que hacerlo de tarea') 
+    def __init__(self, estado_inicial):
+        meta = lambda estado: all(len(set(cara)) == 1 for cara in estado)
+        modelo = CuboRubik()
+        super().__init__(estado_inicial, meta, modelo)
 
 # ------------------------------------------------------------
 #  Desarrolla una política admisible.
@@ -238,7 +311,12 @@ def h_2_MisionerosCanibales(nodo):
     M_izq, C_izq, bote, M_der, C_der = nodo.estado
     return M_izq + (C_izq / 2)  # Prioriza mover misioneros
 
-
+def heuristica_cubo(nodo):
+    """
+    Cuenta el número de piezas mal colocadas en el cubo.
+    """
+    estado = nodo.estado
+    return sum(1 for cara in estado for color in cara if color != cara[0])
 
 def compara_metodos(problema, heuristica_1, heuristica_2):
     """
@@ -300,8 +378,28 @@ if __name__ == "__main__":
     mientras que h2 visita 29, lo que muestra que h1 es mas eficiente que h2 y por lo tanto es dominante.
     '''
 
+    # 7 - Cubo Rubik
     # Compara los métodos de búsqueda para el problema del cubo de rubik
     # con las heurísticas que desarrollaste
-   # problema = PblCuboRubik( XXXXXXXXXX )  # <--- PONLE LOS PARÁMETROS QUE NECESITES
-   # compara_metodos(problema, h_1_problema_1, h_2_problema_1)
+    print("---------- 7 - Cubo Rubik -------------")
+    estado_inicial = [
+        ['A', 'R', 'V', 'N'],  # Cara frontal
+        ['B', 'C', 'A', 'R'],  # Cara lateral derecha
+        ['N', 'V', 'B', 'C'],  # Cara trasera
+        ['R', 'A', 'C', 'V'],  # Cara lateral izquierda
+        ['B', 'B', 'N', 'N'],  # Cara inferior
+        ['C', 'C', 'V', 'V']   # Cara superior
+    ]
+    
+    def meta(estado):
+        return all(len(set(cara)) == 1 for cara in estado)
+
+    problema = PblCuboRubik(estado_inicial)
+    solucion = busquedas.busqueda_A_estrella(problema, heuristica_cubo)
+    if solucion:
+        print("Solución encontrada:")
+        for paso in solucion.camino():
+            print(paso)
+    else:
+        print("No se encontró solución.")
     
