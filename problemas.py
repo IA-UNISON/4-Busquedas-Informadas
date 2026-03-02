014b65f6aacf11810ebfa7eac55e6e85cd26f56e
diegoo_ghz <diego.gallegos.hds@gmail.com>
2026-03-01 22:10:48 -0700

    cubo rubik funcionando por fin

--- problemas.py
+++ problemas.py
@@ -10,6 +10,7 @@ Tarea sobre búsquedas, donde lo que es importante es crear nuevas heurísticas
 
 import busquedas
 import math
+import random
 
 # lo hard codee porque las heuristicas solo reciben el nodo
 # y no quise mover mucho la estructura de los metodos
@@ -117,46 +118,67 @@ class PbCuboRubik(busquedas.ProblemaBusqueda):
     27 28 29         36 37 38         45 46 47
     30 31 32         39 40 41         48 49 50
     33 34 35         42 43 44         51 52 53
+
+            
+               0  1  2
+               3  4  5
+               6  7  8
+
+    45 46 47   18 19 20   36 37 38  27 28 29 
+    48 49 50   21 22 23   39 40 41  30 31 32
+    51 52 53   24 25 26   42 43 44  33 34 35
+
+               9  10 11
+               12 13 14
+               15 16 17
+
     
     0 = blanco(U), 
     1 = amarillo(D), 
     2 = rojo(F), 
     3 = naranja(B), 
-    4 = verde(R), 
-    5 = azul(L)
+    4 = verde(L), 
+    5 = azul(R)
     
     El estado resuelto lo dibuje abajo, en su declaracion
     
     """
 
     MOVIMIENTOS = {
-        'U': [
-            (0,2,8,6), (1,5,7,3),                           # el primer parentesis son las esquinas, el segundo las aristas
-            (18,36,27,45), (19,37,28,46), (20,38,29,47)     # estas son los 12 colores (3 por cara) de las 4 caras q estan pegadas a la que se mueve
-        ],                                                  # un ciclo hace (0, 2, 8, 6) -> (6, 0, 2, 8), por ejemplo
-        'D': [                                              # esto esta dibujado arriba por si no se entiende
-            (9,11,17,15), (10,14,16,12),
-            (24,48,33,42), (25,49,34,43), (26,50,35,44)
-        ],
-        'F': [
-            (18,20,26,24), (19,23,25,21),
-            (6,36,17,45), (7,39,16,48), (8,42,15,51)
-        ],
-        'B': [
-            (27,29,35,33), (28,32,34,30),
-            (0,47,11,38), (1,50,10,41), (2,53,9,44)  
-        ],
-        'R': [
-            (36,38,44,42), (37,41,43,39),
-            (2,27,15,20), (5,30,12,23), (8,33,9,26)  
-        ],
-        'L': [
-            (45,47,53,51), (46,50,52,48),
-            (0,18,11,35), (3,21,14,32), (6,24,17,29) 
-        ],
+    'U': [
+        (6,8,2,0), (3,7,5,1),
+        (38,29,47,20), (37,28,46,19), (36,27,45,18)
+    ],
+    'D': [
+        (15,17,11,9), (12,16,14,10),
+        (53,35,44,26), (52,34,43,25), (51,33,42,24)
+    ],
+    'F': [
+        (24,26,20,18), (21,25,23,19),
+        (47,9,42,6), (50,10,39,7), (53,11,36,8)
+    ],
+    'B': [
+        (33,35,29,27), (30,34,32,28),
+        (38,17,51,0), (41,16,48,1), (44,15,45,2)
+    ],
+    'R': [
+        (42,44,38,36), (39,43,41,37),
+        (20,11,33,2), (23,14,30,5), (26,17,27,8)
+    ],
+    'L': [
+        (51,53,47,45), (48,52,50,46),
+        (35,9,18,0), (32,12,21,3), (29,15,24,6)
+    ],
     }
 
-    ESTADO_RESUELTO = tuple(color for color in range(6) for _ in range(9))
+    ESTADO_RESUELTO = (
+        *[0]*9,  # U = blanco
+        *[1]*9,  # D = amarillo
+        *[2]*9,  # F = rojo
+        *[3]*9,  # B = naranja
+        *[5]*9,  # R = azul
+        *[4]*9,  # L = verde
+    )
     """
     ^ esto genera
 
@@ -164,8 +186,8 @@ class PbCuboRubik(busquedas.ProblemaBusqueda):
     1,1,1,1,1,1,1,1,1,   # cara D, color 1 (amarillo)
     ,2,2,2,2,2,2,2,2,    # cara F, color 2 (rojo)
     3,3,3,3,3,3,3,3,3,   # cara B, color 3 (naranja)
-    4,4,4,4,4,4,4,4,4,   # cara R, color 4 (verde)
-    5,5,5,5,5,5,5,5,5)   # cara L, color 5 (azul)
+    5,5,5,5,5,5,5,5,5,   # cara R, color 5 (azul)
+    4,4,4,4,4,4,4,4,4)   # cara L, color 4 (verde)
 
     9 stickers por cara y todos del mismo color
 
@@ -181,10 +203,10 @@ class PbCuboRubik(busquedas.ProblemaBusqueda):
     def _aplica_ciclos(self, estado, ciclos):
         lista = list(estado)
         for ciclo in ciclos:
-            temp = lista[ciclo[-1]] 
-            for i in range(len(ciclo)-1, 0, -1):
-                lista[ciclo[i]] = lista[ciclo[i-1]]
-            lista[ciclo[0]] = temp
+            temp = lista[ciclo[0]]
+            for i in range(len(ciclo)-1):
+                lista[ciclo[i]] = lista[ciclo[i+1]]
+            lista[ciclo[-1]] = temp
         return tuple(lista)
 
     def acciones(self, estado):
@@ -245,15 +267,15 @@ class PbCuboRubik(busquedas.ProblemaBusqueda):
 
         U, D, F, B, R, L = c
         lineas = [
-            f"                       {U[0]} {U[1]} {U[2]}",
-            f"                       {U[3]} {U[4]} {U[5]}",
-            f"                       {U[6]} {U[7]} {U[8]}",
+            f"        {U[0]} {U[1]} {U[2]}",
+            f"        {U[3]} {U[4]} {U[5]}",
+            f"        {U[6]} {U[7]} {U[8]}",
             f"{L[0]} {L[1]} {L[2]}   {F[0]} {F[1]} {F[2]}   {R[0]} {R[1]} {R[2]}   {B[0]} {B[1]} {B[2]}",
             f"{L[3]} {L[4]} {L[5]}   {F[3]} {F[4]} {F[5]}   {R[3]} {R[4]} {R[5]}   {B[3]} {B[4]} {B[5]}",
             f"{L[6]} {L[7]} {L[8]}   {F[6]} {F[7]} {F[8]}   {R[6]} {R[7]} {R[8]}   {B[6]} {B[7]} {B[8]}",
-            f"                       {D[0]} {D[1]} {D[2]}",
-            f"                       {D[3]} {D[4]} {D[5]}",
-            f"                       {D[6]} {D[7]} {D[8]}",
+            f"        {D[0]} {D[1]} {D[2]}",
+            f"        {D[3]} {D[4]} {D[5]}",
+            f"        {D[6]} {D[7]} {D[8]}",
         ]
         return "\n".join(lineas)
  
@@ -262,12 +284,7 @@ class PbCuboRubik(busquedas.ProblemaBusqueda):
 #  Desarrolla una política admisible.
 # ------------------------------------------------------------
 def h_1_problema_1(nodo):
-    """
-    DOCUMENTA LA HEURÍSTICA QUE DESARROLLES Y DA UNA JUSTIFICACIÓN
-    PLATICADA DE PORQUÉ CREES QUE LA HEURÍSTICA ES ADMISIBLE
-
-    """
-    return 0
+    raise NotImplementedError
 
 
 # ------------------------------------------------------------
@@ -276,13 +293,7 @@ def h_1_problema_1(nodo):
 #  respecto otra política
 # ------------------------------------------------------------
 def h_2_problema_1(nodo):
-    """
-    DOCUMENTA LA HEURÍSTICA DE DESARROLLES Y DA UNA JUSTIFICACIÓN
-    PLATICADA DE PORQUÉ CREES QUE LA HEURÍSTICA ES ADMISIBLE
-
-    """
-    return 0
-
+    raise NotImplementedError
 
 
 def compara_metodos(problema, pos_inicial, heuristica_1, heuristica_2):
@@ -310,15 +321,18 @@ def compara_metodos(problema, pos_inicial, heuristica_1, heuristica_2):
     plan = solucion1.genera_plan()
     for estado, accion, costo in plan:
         print(f"{problema.bonito(estado)} | acción: {accion} | costo: {costo}")
+        print("\n")
 
     print('\nA* con h2'.center(12) 
           + str(solucion2.costo).center(20) 
           + str(solucion2.nodos_visitados)
           + "\n")
     
-    plan = solucion1.genera_plan()
+    plan = solucion2.genera_plan()
     for estado, accion, costo in plan:
         print(f"{problema.bonito(estado)} | acción: {accion} | costo: {costo}")
+        print("\n")
+
     print('-' * 50 + '\n')
 
 
@@ -336,10 +350,10 @@ if __name__ == "__main__":
     # problema = PbCuboRubik( XXXXXXXXXX )  # <--- PONLE LOS PARÁMETROS QUE NECESITES
     # compara_metodos(problema, h_1_problema_1, h_2_problema_1)
     
-    cubo = PbCuboRubik()
-
-    for movimiento in ['U', 'D', 'F', 'B', 'R', 'L']:
+    def revuelve(cubo, n):
+        movimientos = ['U', "U'", 'D', "D'", 'F', "F'", 'B', "B'", 'R', "R'", 'L', "L'"]
         estado = cubo.ESTADO_RESUELTO
-        for _ in range(4):
-            estado, _ = cubo.sucesor(estado, movimiento)
-        print(f"{movimiento} x4 = resuelto: {estado == cubo.ESTADO_RESUELTO}")
+        for _ in range(n):
+            estado, _ = cubo.sucesor(estado, random.choice(movimientos))
+        return estado
+
