f685567bcb723062d7386feb8faeed08826b2794
MelyGM <133719381+MelyGM@users.noreply.github.com>
2026-03-01 17:11:33 -0700

    Implementación del cubo rubik

--- problemas.py
+++ problemas.py
@@ -142,38 +142,176 @@ class PbCuboRubik(busquedas.ProblemaBusqueda):
     https://en.wikipedia.org/wiki/Rubik%27s_Cube
     
     """
-    def __init__(self):
-        raise NotImplementedError('Hay que hacerlo de tarea')
+def __init__(self, estado_inicial=None):
+        self.meta = tuple(
+            [0]*9 + [1]*9 +      
+            [2]*9 + [3]*9 +      
+            [4]*9 + [5]*9        
+        )
+        self.inicial = self.meta if estado_inicial is None else estado_inicial
 
     def acciones(self, estado):
-        raise NotImplementedError('Hay que hacerlo de tarea')
+        return ['arriba', "arriba'", 'frente', "frente'", 'derecha', "derecha'"]
 
     def sucesor(self, estado, accion):
-        raise NotImplementedError('Hay que hacerlo de tarea')
+        if accion == 'arriba':
+            return self._girar_arriba(estado), 1
+        elif accion == "arriba'":
+            return self._girar_arriba_inv(estado), 1
+
+        elif accion == 'frente':
+            return self._girar_frente(estado), 1
+        elif accion == "frente'":
+            return self._girar_frente_inv(estado), 1
+
+        elif accion == 'derecha':
+            return self._girar_derecha(estado), 1
+        elif accion == "derecha'":
+            return self._girar_derecha_inv(estado), 1
+
+        else:
+            raise ValueError("Acción no válida")
 
     def terminal(self, estado):
-        raise NotImplementedError('Hay que hacerlo de tarea')
+        return estado == self.meta
 
     @staticmethod
     def bonito(estado):
-        """
-        El prettyprint de un estado dado
+        print("Arriba:", estado[0:9])
+        print("Derecha:", estado[9:18])
+        print("Frente:", estado[18:27])
+        print("Abajo:", estado[27:36])
+        print("Izquierda:", estado[36:45])
+        print("Atras:", estado[45:54])
 
-        """
-        raise NotImplementedError('Hay que hacerlo de tarea')
- 
+    def _rotar_cara_horario(self, cara):
+        return [
+            cara[6], cara[3], cara[0],
+            cara[7], cara[4], cara[1],
+            cara[8], cara[5], cara[2]
+        ]
 
-# ------------------------------------------------------------
-#  Desarrolla una política admisible.
-# ------------------------------------------------------------
+    def _rotar_cara_antihorario(self, cara):
+        return [
+            cara[2], cara[5], cara[8],
+            cara[1], cara[4], cara[7],
+            cara[0], cara[3], cara[6]
+        ]
+
+    def _girar_arriba(self, estado):
+        s = list(estado)
+
+        s[0:9] = self._rotar_cara_horario(s[0:9])
+
+        frente = s[18:21]
+        derecha = s[9:12]
+        atras = s[45:48]
+        izquierda = s[36:39]
+
+        s[18:21] = izquierda
+        s[9:12] = frente
+        s[45:48] = derecha
+        s[36:39] = atras
+
+        return tuple(s)
+
+    def _girar_arriba_inv(self, estado):
+        s = list(estado)
+
+        s[0:9] = self._rotar_cara_antihorario(s[0:9])
+
+        frente = s[18:21]
+        derecha = s[9:12]
+        atras = s[45:48]
+        izquierda = s[36:39]
+
+        s[18:21] = derecha
+        s[9:12] = atras
+        s[45:48] = izquierda
+        s[36:39] = frente
+
+        return tuple(s)
+    def _girar_frente(self, estado):
+        s = list(estado)
+
+        s[18:27] = self._rotar_cara_horario(s[18:27])
+
+        arriba = [6, 7, 8]        
+        derecha = [9, 12, 15]     
+        abajo = [27, 28, 29]      
+        izquierda = [38, 41, 44]  
+
+        a = [s[i] for i in arriba]
+        d = [s[i] for i in derecha]
+        ab = [s[i] for i in abajo]
+        iz = [s[i] for i in izquierda]
+
+        for j in range(3):
+            s[derecha[j]] = a[j]
+
+        s[abajo[0]], s[abajo[1]], s[abajo[2]] = d[2], d[1], d[0]
+
+        s[izquierda[0]], s[izquierda[1]], s[izquierda[2]] = ab[2], ab[1], ab[0]
+
+        for j in range(3):
+            s[arriba[j]] = iz[j]
+
+        return tuple(s)
+
+
+    def _girar_frente_inv(self, estado):
+        s = estado
+        for _ in range(3):
+            s = self._girar_frente(s)
+        return s
+    
+    def _girar_derecha(self, estado):
+        s = list(estado)
+
+        s[9:18] = self._rotar_cara_horario(s[9:18])
+
+        arriba = [2, 5, 8]    
+        frente = [20, 23, 26]   
+        abajo = [29, 32, 35]    
+        atras = [45, 48, 51]    
+
+        a = [s[i] for i in arriba]
+        f = [s[i] for i in frente]
+        ab = [s[i] for i in abajo]
+        at = [s[i] for i in atras]
+
+        for j in range(3):
+            s[frente[j]] = a[j]
+            s[abajo[j]] = f[j]
+            s[atras[j]] = ab[j]
+            s[arriba[j]] = at[j]
+
+        return tuple(s)
+
+    def _girar_derecha_inv(self, estado):
+        s = estado
+        for _ in range(3):
+         s = self._girar_derecha(s)
+        return s
+    
 def h_1_problema_1(nodo):
     """
     DOCUMENTA LA HEURÍSTICA QUE DESARROLLES Y DA UNA JUSTIFICACIÓN
     PLATICADA DE PORQUÉ CREES QUE LA HEURÍSTICA ES ADMISIBLE
 
     """
-    return 0
+    e = nodo.estado
+    objetivos = [0,1,2,3,4,5]
+    rangos = [(0,9),(9,18),(18,27),(27,36),(36,45),(45,54)]
 
+    mal = 0
+    for cara, (a,b) in enumerate(rangos):
+        color = objetivos[cara]
+        for i in range(a,b):
+            if e[i] != color:
+                mal += 1
+
+    return mal // 8
 
 # ------------------------------------------------------------
 #  Desarrolla otra política admisible.
@@ -186,10 +324,14 @@ def h_2_problema_1(nodo):
     PLATICADA DE PORQUÉ CREES QUE LA HEURÍSTICA ES ADMISIBLE
 
     """
-    return 0
+    e = nodo.estado
+    rangos = [(0,9),(9,18),(18,27),(27,36),(36,45),(45,54)]
 
+    mezcla = 0
+    for a,b in rangos:
+        mezcla += (len(set(e[a:b])) - 1)  
 
-
+    return mezcla
 def compara_metodos(problema, pos_inicial, heuristica_1, heuristica_2):
     """
     Compara en un cuadro lo nodos expandidos y el costo de la solución
@@ -227,9 +369,9 @@ if __name__ == "__main__":
     
     # Compara los métodos de búsqueda para el problema del cubo de rubik
     # con las heurísticas que desarrollaste
-    pos_inicial = XXXXXXXXXX  # <--- PONLE LA POSICIÓN INICIAL QUE QUIERAS
-    problema = PbCuboRubik( XXXXXXXXXX )  # <--- PONLE LOS PARÁMETROS QUE NECESITES
-    compara_metodos(problema, h_1_problema_1, h_2_problema_1)
+    problema = PbCuboRubik()  
+    pos_inicial = problema._girar_arriba(problema.meta) 
+    compara_metodos(problema, pos_inicial, h_1_problema_1, h_2_problema_1)
 
     N = 20
     problema = PbCamionMagico(N)
@@ -244,3 +386,13 @@ if __name__ == "__main__":
     plan, nodos = busquedas.busqueda_costo_uniforme(problema, s0)
     print(plan)
     print("Nodos visitados:", nodos)
+
+    pb = PbCuboRubik()
+    estado0 = pb.meta
+    print("Cubo resuelto:")
+    pb.bonito(estado0)
+
+    estado1, _ = pb.sucesor(estado0, 'frente')
+
+    print("\nDespués de mover frente:")
+    pb.bonito(estado1)
